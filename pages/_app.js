aa07d3af33550826677514882b1aec9ea7b36152
jainsuchi21 <jainsuchi21@gmail.com>
2019-08-27 20:58:52 +0530

    Add polaris and navigation

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>Sample App</title>
+                    <meta charSet="utf-8" />
+                </Head>
+                <AppProvider>
+                    <Component {...pageProps} />
+                </AppProvider>
+            </React.Fragment>
+        );
+    }
+}
+
+export default MyApp;
