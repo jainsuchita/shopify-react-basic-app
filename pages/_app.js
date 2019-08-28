53b5d0565ea003e9d633dca1f710a7c8051bd0ae
jainsuchi21 <jainsuchi21@gmail.com>
2019-08-28 14:03:19 +0530

    Set up Apollo client for graphql queries

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,35 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import '@shopify/polaris/styles.css';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    fetchOptions: {
+        credentials: 'include'
+    },
+});
 
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
+        const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
         return (
             <React.Fragment>
                 <Head>
                     <title>Sample App</title>
                     <meta charSet="utf-8" />
                 </Head>
-                <AppProvider>
-                    <Component {...pageProps} />
-                </AppProvider>
+                <Provider config={config}>
+                    <AppProvider>
+                        <ApolloProvider client={client}>
+                            <Component {...pageProps} />
+                        </ApolloProvider>
+                    </AppProvider>
+                </Provider>
             </React.Fragment>
         );
     }
