efa7766faac598803b050879aec7dcca669ca3b9
jainsuchi21 <jainsuchi21@gmail.com>
2019-08-28 14:16:14 +0530

    Add graphql  mutation

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -8,6 +8,8 @@ import {
   Thumbnail,
 } from '@shopify/polaris';
 import store from 'store-js';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -39,8 +41,17 @@ const GET_PRODUCTS_BY_ID = gql`
 `;
 
 class ResourceListWithProducts extends React.Component {
+  static contextType = Context;
   render() {
     const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
+    const app = this.context;
+    const redirectToProduct = () => {
+      const redirect = Redirect.create(app);
+      redirect.dispatch(
+        Redirect.Action.APP,
+        '/edit-products',
+      );
+    };
     return (
       // <Query query={GET_PRODUCTS_BY_ID}>
       <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
@@ -76,6 +87,10 @@ class ResourceListWithProducts extends React.Component {
                         id={item.id}
                         media={media}
                         accessibilityLabel={`View details for ${item.title}`}
+                        onClick={() => {
+                          store.set('item', item);
+                          redirectToProduct();
+                        }}
                       >
                         <Stack>
                           <Stack.Item fill>
